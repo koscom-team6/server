9f7cea0fce829ade5c290d70e6a7f0fedf80338e
ap3334 <chwil2989@naver.com>
2025-02-04 19:11:13 +0900

    [fix] match auto incremente 설정

--- src/main/java/koscom/team6/domain/match/Entity/Matching.java
+++ src/main/java/koscom/team6/domain/match/Entity/Matching.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class Matching {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
